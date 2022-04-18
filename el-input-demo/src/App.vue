774e2dfb2041c15b4a8d43985aaea05b46c21f5f
shaoxueping <3059680677@qq.com>
2022-04-18 16:57:38 +0800

    feat: compelte the el-input

---
+++ el-input-demo/src/App.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <div>来点文字：<ElInput v-model="inputValue" /></div>
+    <hr />
+    纯文本 : <ElInput onlyShowText>纯文本纯文本....</ElInput>
+    <hr />
+    text: <ElInput />同行的第二个 <ElInput width="500px" />
+    <hr />
+    复选框 :
+    <ElInput
+      type="checkbox"
+      v-for="(item, index) of checkedList"
+      :key="index"
+      :disabled="index == 3"
+      >checkbox - {{ index }}
+    </ElInput>
+    <hr />
+    单选框：
+    <ElInput type="radio" v-model="radioValue" label="1">一</ElInput>
+    <ElInput type="radio" v-model="radioValue" label="2">二</ElInput>
+    <ElInput type="radio" v-model="radioValue" label="3">三</ElInput>
+    <ElInput type="radio" v-model="radioValue" label="4" disabled>四</ElInput>
+    <hr />
+    两个文本框:
+    <ElInput width="300px" /><ElInput width="300px" />
+    <hr />
+    三个文本框
+    <ElInput width="200px" /><ElInput width="200px" /><ElInput width="200px" />
+    <hr />
+    下拉框
+    <ElInput
+      width="200px"
+      type="select"
+      v-model="selectValue"
+      placeholder="请选择"
+      :options="selectOptions"
+      option-key="label"
+      option-value="value"
+    />
+    <hr />
+    文本框：
+    <ElInput type="textarea" v-model="textareaValue" placeholder="初始化" />
+  </div>
+</template>
+
+<script>
+import ElInput from "./components/ElInput.vue";
+export default {
+  name: "App",
+  components: {
+    ElInput,
+  },
+  data() {
+    return {
+      inputValue: "",
+      checkedList: [
+        { id: 1, checked: false },
+        { id: 2, checked: false },
+        { id: 3, checked: false },
+        { id: 4, checked: false },
+      ],
+      radioValue: "1",
+      textareaValue: "",
+      selectValue: "",
+      selectOptions: [
+        {
+          value: "选项1",
+          label: "黄金糕",
+        },
+        {
+          value: "选项2",
+          label: "双皮奶",
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎",
+        },
+        {
+          value: "选项4",
+          label: "龙须面",
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleInput(e) {
+      this.inputValue = e.target.value;
+    },
+    handleCheck(e) {
+      this.checked1 = !this.checked1;
+    },
+  },
+};
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ el-input-demo/src/components/ElInput.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="el-input__wrapper">
+    <span v-if="textOnly">
+      <slot>{{ value }}</slot>
+    </span>
+    <div class="el-input__text" v-if="isShowText">
+      <input
+        :name="name"
+        :type="type"
+        :value="value"
+        :style="{ width }"
+        @input="handleInput"
+      />
+    </div>
+    <div class="el-input__checkbox" v-if="isShowCheckbox">
+      <label>
+        <input
+          :name="name"
+          :type="type"
+          :checked="value"
+          :style="{ width }"
+          :class="{ 'is-disabled': disabled }"
+          :disabled="disabled"
+          @change="handleCheck"
+        />
+        <slot></slot>
+      </label>
+    </div>
+    <div class="el-input__radio" v-show="isShowRadio">
+      <label>
+        <input
+          :name="name"
+          :type="type"
+          :value="label"
+          :disabled="disabled"
+          :checked="disabled || label === value"
+          @change="handleInput"
+        />
+        <slot></slot>
+      </label>
+    </div>
+    <div class="el-input__select" v-show="isShowSelect">
+      <select
+        :disabled="disabled"
+        :name="name"
+        :placeholder="placeholder"
+        :style="{ width }"
+        :value="value"
+        @change="handleInput"
+      >
+        <option
+          :value="item[optionValue]"
+          v-for="item in options"
+          :key="item[optionValue]"
+        >
+          {{ item[optionKey] }}
+        </option>
+      </select>
+    </div>
+    <div class="el-input__textarea" v-show="isShowTextarea">
+      <textarea
+        :name="name"
+        :value="value"
+        :cols="cols"
+        :rows="rows"
+        @input="handleInput"
+        :placeholder="placeholder"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ElInput",
+  props: {
+    type: {
+      type: String,
+      default: "text",
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: [String, Number, Boolean],
+      default: "",
+    },
+    label: {
+      type: [String, Number],
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    options: {
+      type: Array,
+      default: () => [],
+    },
+    optionKey: {
+      type: String,
+      default: "",
+    },
+    optionValue: {
+      type: String,
+      default: "",
+    },
+    textOnly: {
+      type: Boolean,
+      default: false,
+    },
+    rows: {
+      type: Number,
+      default: 10,
+    },
+    cols: {
+      type: Number,
+      default: 50,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    width: String,
+  },
+  computed: {
+    isShowText() {
+      return this.type === "text" && !this.textOnly;
+    },
+    isShowCheckbox() {
+      return this.type === "checkbox" && !this.textOnly;
+    },
+    isShowRadio() {
+      return this.type === "radio" && !this.textOnly;
+    },
+    isShowSelect() {
+      return this.type === "select" && !this.textOnly;
+    },
+    isShowTextarea() {
+      return this.type === "textarea" && !this.textOnly;
+    },
+  },
+  methods: {
+    handleInput(e) {
+      this.$emit("input", e.target.value);
+    },
+    handleCheck(e) {
+      this.$emit("input", e.target.checked);
+    },
+  },
+};
+</script>
+
+<style lang='less' scoped>
+.el-input__wrapper {
+  display: inline-block;
+  .el-input__text {
+    display: inline-block;
+    input {
+      border: 1px solid #ddd;
+      margin-right: 10px;
+      border-radius: 5px;
+      height: 20px;
+    }
+  }
+  .el-input__checkbox {
+    display: inline-block;
+    margin-right: 5px;
+  }
+  .el-input__textarea {
+  }
+}
+</style>
